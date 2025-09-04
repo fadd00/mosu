cfd19577c708da15b569c17b2e13dd65908930e9
fadd00 <andhikahutama9@gmail.com>
2025-09-04 23:13:51 +0700

    Remove unnecessary whitespace in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { ChevronDown } from "lucide-react";
-
-"use client";
-import React, { useState } from "react";
-import { ChevronDown } from "lucide-react";
 import Link from "next/link";
 
 export default function Navbar() {
